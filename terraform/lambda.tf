ed466c444e7c04ba9f10da369c0e9b06b9cdecad
Dom Weldon <dom.weldon@gmail.com>
2021-01-16 23:11:57 +0000

    Adding CI workflow

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,10 @@
 module "bindicator_lambda" {
-  source           = "./lambda"
-  project_name     = var.project_name
-  environment_name = var.environment_name
-  fn_name          = var.fn_name
-  fn_handler       = var.fn_handler
+  source                    = "./lambda"
+  project_name              = var.project_name
+  environment_name          = var.environment_name
+  fn_name                   = var.fn_name
+  fn_handler                = var.fn_handler
+  lambda_source_bucket_name = "domweldon-bindicator-lambda-source-01"
 
   environment_variables = [{
     # project
